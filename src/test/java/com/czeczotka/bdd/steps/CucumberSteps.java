a57a4dff2ec43f63b977c1062a7282c2b3420a55
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2014-08-16 22:48:38 +0100

    Add exact match step

--- src/test/java/com/czeczotka/bdd/steps/CucumberSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CucumberSteps.java
@@ -2,10 +2,6 @@ package com.czeczotka.bdd.steps;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
-import static java.lang.String.format;
 
 public class CucumberSteps {
 
@@ -13,17 +9,8 @@ public class CucumberSteps {
     public void setUp() {
     }
 
-    @Given("^I have a calculator$")
-    public void i_have_a_calculator() throws Throwable {
+    @Given("^I have a cucumber step$")
+    public void i_have_a_cucumber_step() throws Throwable {
         System.out.println ("We have an exact match on the step name");
-    }
-
-    @When("^I add (\\d+) and (\\d+)$")
-    public void i_add_and(int arg1, int arg2) throws Throwable {
-        System.out.println (format("Getting integers {0} and {1}", arg1, arg2));
-    }
-
-    @Then("^the result should be (\\d+)$")
-    public void the_result_should_be(int result) throws Throwable {
     }
 }
--- src/test/resources/cucumber/regex.feature
+++ src/test/resources/cucumber/regex.feature
@@ -2,3 +2,6 @@ Feature: Regular expressions in step definitions
   As a developer
   I want to see examples of regular expressions in step definitions
   So that I can write powerful Cucumber steps
+
+  Scenario: Exact match
+    Given I have a cucumber step
